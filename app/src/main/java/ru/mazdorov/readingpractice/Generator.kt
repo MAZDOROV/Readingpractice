1a775899529794e4b290ea4555348777d86ab460
mazdorovis <mazdorov.is@gmail.com>
2023-03-21 22:24:48 +0600

    level 4 exceptions added. Refactor

--- app/src/main/java/ru/mazdorov/readingpractice/Generator.kt
+++ app/src/main/java/ru/mazdorov/readingpractice/Generator.kt
@@ -3,15 +3,19 @@ package ru.mazdorov.readingpractice
 import kotlin.random.Random
 
 class Generator {
-
     private val allLetters = ("АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЧШЩЪЫЬЭЮЯ").toCharArray()
     private val vowels = ("АЕЁИОУЫЭЮЯ").toCharArray()
     private val consonants = ("БВГДЖЗЙКЛМНПРСТФХЦШЩ").toCharArray()
     private val other = ("ЪЬ").toCharArray()
-    private val exceptions = arrayOf<String>("ЖЫ", "ШЫ", "ЩЯ", "ЧЯ", "ЩЮ", "ЧЮ", "ЙЯ", "ЙЮ", "ЙЁ", "ЙУ", "ЙО", "ЙА","ЙИ","ЙЫ","ЙЭ","ЙЕ")
+    private val exceptions = arrayOf<String>(
+        "ЖЫ", "ЖЯ", "ЖЮ", "ЖЭ",
+        "ЩЫ", "ЩЯ", "ЩЮ", "ЩЭ",
+        "ШЫ", "ШЯ", "ШЮ", "ШЁ",
+        "ЧЫ", "ЧЯ", "ЧЮ", "ЧЁ",
+        "ЙЯ", "ЙЮ", "ЙЁ", "ЙУ", "ЙО", "ЙА", "ЙИ", "ЙЫ", "ЙЭ", "ЙЕ")
     private val randomizer = Randomizer()
 
-    private fun getOneRandom(charArray: CharArray): CharSequence {
+    private fun getRandomLater(charArray: CharArray): CharSequence {
         val random = Random(randomizer.randomNumber)
         val next = random.nextInt( charArray.size)
         return charArray[next].toString()
@@ -19,9 +23,9 @@ class Generator {
 
     fun generate(level: Int): CharSequence {
         return when(level){
-            0-> getOneRandom(vowels).toString()
-            1-> getOneRandom(consonants).toString()
-            2-> getOneRandom(allLetters).toString()
+            0-> getRandomLater(vowels).toString()
+            1-> getRandomLater(consonants).toString()
+            2-> getRandomLater(allLetters).toString()
             3-> getLevel_3_Text()
             4-> getLevel_4_Text()
             else -> {
@@ -30,15 +34,20 @@ class Generator {
         }
     }
 
-    fun getLevel_3_Text(): String
-    {
-        var result = getOneRandom(consonants).toString() + getOneRandom(vowels).toString()
+    fun getLevel_3_Text(): String {
+        var result = getRandomLater(consonants).toString() + getRandomLater(vowels).toString()
         if (exceptions.contains(result))
             result = getLevel_3_Text()
         return result
     }
 
     private fun getLevel_4_Text(): CharSequence {
-        return getLevel_3_Text() + "-" + getLevel_3_Text()
+        val firstPart = getLevel_3_Text()
+        var secondPart: String
+        do {
+            secondPart = getLevel_3_Text()
+        }while(firstPart[0] == secondPart[0] || firstPart[1] == secondPart[1] )
+
+        return "$firstPart-$secondPart"
     }
 }
